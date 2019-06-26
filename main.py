4845a2be7038f1c54920972f978397a26581b339
Denys <denis.lenskiy1998@gmail.com>
2019-06-26 14:47:56 +0300

    added examples if the form of figures and csv files

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ import time
 import traceback
 
 # Importing libraries for ADS1115 connection
-import board
+# import board
 import busio
 import adafruit_ads1x15.ads1115 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
